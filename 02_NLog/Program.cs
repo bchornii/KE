66bd91480259ae2d6c5a36b92dbab83a4d7ad988
bchornii <bogdan.chorniy@gmail.com>
2017-03-10 14:45:59 +0200

    Contracts, AppDomain, Event logging and NLog

---
+++ 02_NLog/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using NLog;
+
+namespace _02_NLog
+{
+    public class TestLoggerClass
+    {
+        private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+
+        public void WriteLog()
+        {
+            const int k = 42;
+            const int l = 100;
+
+            Logger.Trace("Sample trace message, k={0}, l={1}", k, l);
+            Logger.Debug("Sample debug message, k={0}, l={1}", k, l);
+            Logger.Info("Sample informational message, k={0}, l={1}", k, l);
+            Logger.Warn("Sample warning message, k={0}, l={1}", k, l);
+            Logger.Error("Sample error message, k={0}, l={1}", k, l);
+            Logger.Fatal("Sample fatal error message, k={0}, l={1}", k, l);
+            Logger.Log(LogLevel.Info, "Sample informational message, k={0}, l={1}", k, l);
+        }
+    }
+
+    internal class Program
+    {
+        private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+        private static void Main()
+        {
+            var k = 42;
+            var l = 100;
+            var testLogger = new TestLoggerClass();
+
+            testLogger.WriteLog();
+
+            Console.WriteLine(new string('-', 80));
+            Logger.Trace("Sample trace message, k={0}, l={1}", k, l);
+            Logger.Debug("Sample debug message, k={0}, l={1}", k, l);
+            Logger.Info("Sample informational message, k={0}, l={1}", k, l);
+            Logger.Warn("Sample warning message, k={0}, l={1}", k, l);
+            Logger.Error("Sample error message, k={0}, l={1}", k, l);
+            Logger.Fatal("Sample fatal error message, k={0}, l={1}", k, l);
+            Logger.Log(LogLevel.Info, "Sample informational message, k={0}, l={1}", k, l);
+
+            Console.WriteLine("Press any <Enter>...");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ _01_CodeContracts/Program.cs
@@ -0,0 +1,68 @@
+﻿
+//#define TEST
+//#define VERIFY
+
+using System;
+using System.Diagnostics;
+using System.Diagnostics.Contracts;
+using System.Reflection;
+
+namespace _01_CodeContracts
+{
+    [Conditional("TEST")]
+    [Conditional("VERIFY")]
+    public sealed class CondAttribute : Attribute
+    {
+    }
+
+    [Cond]
+    internal class Program
+    {
+        static Program()
+        {
+            AppDomain.CurrentDomain.FirstChanceException +=
+                (sender, args) => { Console.WriteLine(args.Exception.Message); };
+        }
+
+        private static void Main()
+        {
+            var logName = "ProCSharpLog";
+            var sourceName = "EventLogDemoApp";
+
+            //if (!EventLog.SourceExists(sourceName))
+            //{
+            //    var eventSourceData = new EventSourceCreationData(sourceName, logName);
+            //    EventLog.CreateEventSource(eventSourceData);
+            //}
+
+            //using (var log = new EventLog(logName,".", sourceName))
+            //{
+            //    log.WriteEntry("Message 1");
+            //    log.WriteEntry("Message 2", EventLogEntryType.Warning);
+            //    log.WriteEntry("Message 2", EventLogEntryType.Information, 33);
+            //}
+
+            //foreach (var performanceCounterCategory in PerformanceCounterCategory.GetCategories("."))
+            //{
+            //    Console.WriteLine(performanceCounterCategory.CategoryName);
+            //}
+
+            Console.WriteLine("CondAttribute is {0}applied to Program type.",
+                Attribute.IsDefined(typeof(Program), typeof(CondAttribute))
+                    ? ""
+                    : "not ");
+
+            Console.ReadLine();
+        }
+
+        private static void Except()
+        {
+            throw new ArgumentException("sgsgsg");
+        }
+
+        private static void MinMax(int min, int max)
+        {
+            Contract.Requires<ArgumentException>(min < max);
+        }
+    }
+}
