7bb8c7885a3ae3189e1171cdd848059a3ea343b1
bchornii <bogdan.chorniy@gmail.com>
2017-04-06 21:08:23 +0300

    Added iterator proj.

---
+++ 01_Iterators/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+
+namespace _01_Iterators
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+        }
+
+        private static IEnumerator GetCounter()
+        {
+            for (var count = 0; count < 10; count++)
+            {
+                yield return count;
+            }
+
+            var a = 10;
+            switch (a)
+            {
+                case 5:
+                    yield return 5;
+                    break;
+                case 10:
+                    yield return 10;
+                    break;
+            }
+        }
+    }
+}
